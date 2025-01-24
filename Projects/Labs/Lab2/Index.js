0b7b4b608b716795c5770b2719ae8d2d48ae628e
Sally <sallysomo74@gmail.com>
2025-01-24 18:41:00 -0500

    lab2

---
+++ Projects/Labs/Lab2/Index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import lab_router from "./routers/lab_router.js"
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use("/lab", lab_router);
+// localhost:8000/lab
+
+app.get("/", (req, res)=>{
+    res.send("Welcome to the Server")
+})
+
+app.listen(PORT, ()=>{
+    console.log('http://localhost:${PORT}')
+})
